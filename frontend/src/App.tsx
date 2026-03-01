20a9ecd3bf7d5327233b80fca10541a493fa46fe
Bogomil Petorv <bogopetrov07@gmail.com>
2026-03-01 15:27:51 +0200

    build: Add Vecel Speed Insights to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,21 @@
 //import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Analytics } from "@vercel/analytics/react"
+import {Analytics} from "@vercel/analytics/react"
+import {SpeedInsights} from "@vercel/speed-insights/react"
 
 function App() {
 
 
-  return (
-      <>
-      <Analytics />
-      </>
-    // <Router>
-    //     <Routes>
-    //
-    //     </Routes>
-    // </Router>
-  );
+    return (
+        <>
+            <Analytics/>
+            <SpeedInsights/>
+        </>
+        // <Router>
+        //     <Routes>
+        //
+        //     </Routes>
+        // </Router>
+    );
 }
 
 export default App;
